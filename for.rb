f5558a07a3eec45d27ed85b839c6408a8f5e7a6b
mjm03 <mjm03@hampshire.edu>
2019-03-28 00:44:53 +0000

    Done.

--- for.rb
+++ for.rb
@@ -1,7 +1,5 @@
-
 def using_for
-	checklist = 0
-	while checklist < 10 do
-	  puts "Wingardium Leviosa"
-	  checklist += 1
+	checklist = 1..10
+	for item in checklist
+	puts "Wingardium Leviosa"
 end
